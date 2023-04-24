2aca2437aec27034ec2c12e94ea054222479921b
Alissonn1325 <alissonfbrito01@gmail.com>
2023-04-24 11:50:43 -0300

    Exercicio1&2

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref }from 'vue'
+
+const cidades = ref ([
+    'São Paulo',
+    'Rio de Janeiro',
+    'Belo Horizonte',
+    'Salvador',
+    'Fortaleza',
+    'Curitiba',
+    'Manaus',
+    'Recife',
+    'Porto Alegre',
+    'Brasília'
+]
+)
+</script>
+
+<template>
+  <div class="tema">
+   <ul>
+    <h1>Cidades Gamers</h1>
+    <hr>
+    <li v-for="(cidade, index) in cidades.sort()" :key="index">{{ cidade }}</li>
+  </ul>
+  </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Bruno+Ace+SC&display=swap');
+.tema {
+  background-color: rgb(0, 0, 0);
+  padding: 20px 30px;
+  color: rgb(183, 210, 219);
+  border-radius: 10px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  width: 28%;
+  margin: 0 auto;
+  margin-top: 12%;
+  margin-bottom: 20px;
+}
+
+h1 {
+  color: white;
+  font-weight: bold;
+  font-size: 1.5em;
+  font-family: 'Bruno Ace SC', cursive;
+}
+
+li {
+  margin: 15px 15px 15px 15px;
+  color: white;
+  font-family: 'Bruno Ace SC', cursive;
+}
+</style>
